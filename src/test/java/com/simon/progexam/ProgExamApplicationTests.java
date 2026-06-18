9433aff91af80a6a040a8053e5656e105d01a5c3
SimAbild <abildtrup123@gmail.com>
2026-06-18 10:03:04 +0200

    Opsætning af projekt

---
+++ src/test/java/com/simon/progexam/ProgExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simon.progexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
